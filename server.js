d09fa74fce7b855c23a985e0d97aa7d79f742216
mannyrveloz23 <you@example.com>
2023-02-20 06:47:51 -0500

    routes created

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/api", apiroutes);
+app.use("/", htmlroutes)
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server available at localhost${PORT}`);
+  });
+
